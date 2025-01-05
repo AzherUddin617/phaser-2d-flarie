412128f7168363749b1513793b27d60e40868a67
Azher Uddin <azher305uddin@gmail.com>
2025-01-05 20:03:19 +0600

    update: platforms added for game

--- src/game/gameobjects/Platforms.ts
+++ src/game/gameobjects/Platforms.ts
@@ -4,5 +4,12 @@ export default class Platforms extends Phaser.Physics.Arcade.StaticGroup {
         super(scene.physics.world, scene);
 
         this.create(0, this.scene.scale.height, 'ground').setOrigin(0, 1).setScale(2).refreshBody();
+
+        this.create(200, this.scene.scale.height - 200, 'ground')
+        this.create(750, this.scene.scale.height - 300, 'ground')
+        this.create(100, this.scene.scale.height - 350, 'ground')
+        this.create(0, this.scene.scale.height - 500, 'ground')
+        this.create(-150, this.scene.scale.height - 600, 'ground')
+        this.create(850, this.scene.scale.height - 450, 'ground')
     }
 }
