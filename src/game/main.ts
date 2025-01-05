bbd37d0fb29d8b15c36dbd10fd23e4c32306423c
Azher Uddin <azher305uddin@gmail.com>
2025-01-05 19:59:54 +0600

    add: game scene with game mechanics update: main config file

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,24 +1,31 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import { MainMenu } from './scenes/MainMenu';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 720,
+    height: 1280,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300, x: 0 },
+            debug: true
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT, // to fit within the screen
+        autoCenter: Phaser.Scale.CENTER_BOTH // to center the game
+    },
     scene: [
-        Boot,
         Preloader,
         MainMenu,
-        MainGame,
-        GameOver
+        MainGame
     ]
 };
 
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,11 +1,19 @@
-import { EventBus } from '../EventBus';
 import { Scene } from 'phaser';
+import { EventBus } from '../EventBus';
+import Player from '../gameobjects/Player';
+import Platforms from '../gameobjects/Platforms';
+import Stars from '../gameobjects/Stars';
 
 export class Game extends Scene
 {
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    gameText: Phaser.GameObjects.Text;
+    platforms: Phaser.Physics.Arcade.StaticGroup;
+    player: Phaser.Physics.Arcade.Sprite;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    stars: Phaser.Physics.Arcade.Group;
+    bombs: Phaser.Physics.Arcade.Group;
+
+    score = 0;
+    scoreText: Phaser.GameObjects.Text;
 
     constructor ()
     {
@@ -14,23 +22,89 @@ export class Game extends Scene
 
     create ()
     {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-
         EventBus.emit('current-scene-ready', this);
+
+        this.add.image(0, 0, 'sky').setOrigin(0, 0).setScale(2.1);
+
+        // this.platforms = this.physics.add.staticGroup();
+        this.platforms = new Platforms(this);
+
+        this.player = new Player(this, 100, this.scale.height - 150);
+
+        this.stars = new Stars(this);
+
+        this.bombs = this.physics.add.group();
+
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', backgroundColor: '#000' });
+
+        //  Collide the player and the stars with the platforms
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.collider(this.bombs, this.platforms);
+
+        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, undefined, this);
+
+        this.physics.add.overlap(this.player, this.bombs, this.hitBomb, undefined, this);
+    }
+
+    update ()
+    {
+        this.player.update();
+    }
+
+    collectStar: Phaser.Types.Physics.Arcade.ArcadePhysicsCallback = (player, star) =>
+    {
+        (star as unknown as Phaser.Physics.Arcade.Image)?.disableBody(true, true);
+
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+
+        if (this.stars.countActive(true) === 0)
+            {
+                //  A new batch of stars to collect
+                this.stars.children.iterate((child) => {
+
+                    if (child instanceof Phaser.Physics.Arcade.Sprite) {
+        
+                        child.enableBody(true, child.x, 0, true, true);
+            
+                        return true;
+                    }
+
+                    return false;
+                });
+        
+                const x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        
+                const bomb = this.bombs.create(x, 16, 'bomb');
+                bomb.setBounce(1);
+                bomb.setCollideWorldBounds(true);
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+                bomb.allowGravity = false;
+        
+            }
+    }
+    
+    hitBomb: Phaser.Types.Physics.Arcade.ArcadePhysicsCallback = (player) =>
+    {
+        this.physics.pause();
+
+        (player as unknown as Phaser.Physics.Arcade.Image)?.setTint(0xff0000);
+
+        (player as unknown as Phaser.Physics.Arcade.Sprite)?.anims.play('turn');
+
+        this.changeScene();
     }
 
     changeScene ()
     {
-        this.scene.start('GameOver');
+        // pause game
+        this.scene.pause('Game');
+
+        setTimeout(() =>
+        {
+            this.scene.start('MainMenu');
+        }, 1000);
     }
 }
