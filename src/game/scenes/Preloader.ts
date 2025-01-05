0d2fcc590b3a7a77349d5f60e2f124f11119f804
Azher Uddin <azher305uddin@gmail.com>
2025-01-05 19:55:00 +0600

    feat: preload scene for loading all assets

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import { EventBus } from '../EventBus';
 
 export class Preloader extends Scene
 {
@@ -9,14 +10,11 @@ export class Preloader extends Scene
 
     init ()
     {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
-
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(this.cameras.main.width / 2, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(this.cameras.main.width / 2 - 230, 384, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress: number) => {
@@ -32,12 +30,18 @@ export class Preloader extends Scene
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
-        this.load.image('logo', 'logo.png');
+        this.load.image('bg-menu', 'bg.jpg');
+
+        this.load.image('sky', 'sky.png');
+        this.load.image('ground', 'platform.png');
         this.load.image('star', 'star.png');
+        this.load.image('bomb', 'bomb.png');
+        this.load.spritesheet('dude', 'dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     create ()
     {
+        EventBus.emit('current-scene-ready', this);
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, you can define global animations here, so we can use them in other scenes.
 
